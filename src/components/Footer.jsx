cdb710731790b3fd8ec3c258cb36df8dfee23663
Bappy Modok <121638025+Bappy00111@users.noreply.github.com>
2024-02-29 10:53:26 +0600

    footer and hero section added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-6 md:py-8">
+      <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
+        Copyright ©2024 | All rights reserved by Learn with Sumit
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
